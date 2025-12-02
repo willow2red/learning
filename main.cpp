25e80baeac3f082c6686ef7177620d956b3b9679
willow2red <willow2t@ioiio.cc>
2025-12-02 18:29:49 +0700

    c

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <array>
 #include <iostream>
 
+template<typename T, std::size_t N>
+void passByRef(const std::array<T, N>& arr)
+{
+    // static_assert(N > 3);
+    static_assert(std::size(arr) > 3);
+    std::cout << arr[0] << "\n";
+}
+
 int main() {
     std::cout << "Test" << "\n";
     std::array<int, 3> arr = {1, 2, 5};
@@ -10,6 +18,8 @@ int main() {
     constexpr int y {x};
     std::cout << y << "\n";
 
+    std::array<int, 5> arr5 {1, 2, 3, 4, 5};
+    passByRef(arr5);
 
     return 0;
 }
